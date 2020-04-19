366de1ee32acd3a2bac0659807ac08ce3f708768
Y Paul Sussman <ypaulsussman@gmail.com>
2020-04-19 15:46:48 -0500

    comment out studies fk

--- add_primary_foreign_keys.sql
+++ add_primary_foreign_keys.sql
@@ -10,8 +10,9 @@ ALTER TABLE findings RENAME COLUMN findingid TO id;
 
 ALTER TABLE intervention_reports ADD COLUMN id SERIAL PRIMARY KEY;
 
-ALTER TABLE findings ADD CONSTRAINT constraint_fk_studies FOREIGN KEY (reviewid) REFERENCES studies (id);
--- NB This constraint won't work, currently, because findings [8504, 8494, 8491, 8501] reference the not-present study 1902428; the underlying intervention, 1298, was instead the subject of studies [1902429, 1902435, 1902438, 1902458, 1902433, 1902439, 1902459, 1902431, 1902434]
+-- ALTER TABLE findings ADD CONSTRAINT constraint_fk_studies FOREIGN KEY (reviewid) REFERENCES studies (id);
+-- NB This constraint won't work, currently, because e.g. findings [8504, 8494, 8491, 8501] reference the not-present study 1902428; the underlying intervention, 1298, was instead the subject of studies [1902429, 1902435, 1902438, 1902458, 1902433, 1902439, 1902459, 1902431, 1902434]
+-- Unfortunately, a test-deletion of those four findings revealed that this problem isn't limited to a single intervention. As such -- irksome as it is -- I've commented out the above.
 
 ALTER TABLE findings RENAME COLUMN reviewid TO study_id;
 
